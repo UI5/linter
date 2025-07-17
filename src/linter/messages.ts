420c61f37c023b10fc7223036e75b688d3a5091a
Yavor Ivanov <d3xter666@users.noreply.github.com>
2025-07-17 14:15:48 +0300

    docs: Update src/linter/messages.ts
    Co-authored-by: Günter Klatt <57760635+KlattG@users.noreply.github.com>

--- src/linter/messages.ts
+++ src/linter/messages.ts
@@ -733,7 +733,7 @@ export const MESSAGE_INFO = {
 			`Property '${propName}' has been renamed to '${newName}' in Manifest Version 2`,
 		details: ({propName, newName}: {propName: string; newName: string}) =>
 			`Rename property '${propName}' to '${newName}' and follow the migration guide ` +
-			`{@link topic:be0cf40f61184b358b5faedaec98b2da Manifest (Descriptor for Applications, ` +
+			`{@link topic:be0cf40f61184b358b5faedaec98b2da#loiobe0cf40f61184b358b5faedaec98b2da/section_manifest2 Manifest ` +
 			`Components, and Libraries)}`,
 	},
 
