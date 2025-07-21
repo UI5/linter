a5933aa0bb2e7b1da4e69f050878037a87bf8f93
Merlin Beutlberger <m.beutlberger@sap.com>
2025-07-21 10:47:38 +0900

    feat: Export 'Ui5TypeInfoMatcher' module
    Also provide the "mocked" type info for SAP-jQuery API in the linter
    results.

--- test/e2e/package-exports.ts
+++ test/e2e/package-exports.ts
@@ -11,12 +11,18 @@ test.serial("Package exports: export of package.json", (t) => {
 // Check number of defined exports
 test.serial("Package exports: check number of exports", (t) => {
 	const packageJson = require("@ui5/linter/package.json");
-	t.is(Object.keys(packageJson.exports).length, 2);
+	t.is(Object.keys(packageJson.exports).length, 3);
 });
 
 // Public API contract (exported modules)
 test.serial("Package exports: @ui5/linter", async (t) => {
 	const actual = await import("@ui5/linter");
 	const expected = await import("../../lib/index.js");
+	t.is(actual, expected, "Correct module exported");
+});
+
+test.serial("Package exports: @ui5/linter/Ui5TypeInfoMatcher", async (t) => {
+	const actual = await import("@ui5/linter/Ui5TypeInfoMatcher");
+	const expected = await import("../../lib/linter/ui5Types/Ui5TypeInfoMatcher.js");
 	t.is(actual, expected, "Correct module exported");
 });
