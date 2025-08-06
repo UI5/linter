f0c71637b5d3d7f023dbd030ef952bd3e904d647
d3xter666 <yavor.ivanov@sap.com>
2025-08-06 10:50:30 +0300

    refactor: ManifestLinter only for manifest.json files

--- src/linter/manifestJson/ManifestLinter.ts
+++ src/linter/manifestJson/ManifestLinter.ts
@@ -43,6 +43,11 @@ export default class ManifestLinter {
 
 	// eslint-disable-next-line @typescript-eslint/require-await
 	async lint() {
+		// Only lint manifest.json files and skip manifest.appdescr_variant, for example
+		if (!this.#resourcePath.includes("manifest.json")) {
+			return;
+		}
+
 		try {
 			const source = this.#parseManifest(this.#content);
 			this.#reporter = new ManifestReporter(this.#resourcePath, this.#context, source);
