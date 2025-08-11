db38138b5311f956f11f2183a9952f8cdaec4566
d3xter666 <yavor.ivanov@sap.com>
2025-08-11 16:20:01 +0300

    feat: Implement method detection inside EventHandlers

--- src/linter/ui5Types/fix/EventHandlersFix.ts
+++ src/linter/ui5Types/fix/EventHandlersFix.ts
@@ -1,5 +1,5 @@
-import ts, { Program } from "typescript";
-import {ChangeSet} from "../../../autofix/autofix.js";
+import ts, {Program} from "typescript";
+import {ChangeAction, ChangeSet} from "../../../autofix/autofix.js";
 import {PositionInfo} from "../../LinterContext.js";
 import {Attribute, Position, SaxEventType} from "sax-wasm";
 import XmlEnabledFix from "./XmlEnabledFix.js";
@@ -11,6 +11,7 @@ export default class EventHandlersFix extends XmlEnabledFix {
 	protected startPos: number | undefined;
 	protected endPos: number | undefined;
 	protected trailingCommaPos: number | undefined;
+	protected methodName: string | undefined;
 
 	constructor() {
 		super();
@@ -32,17 +33,16 @@ export default class EventHandlersFix extends XmlEnabledFix {
 		};
 	}
 
-	// TODO:
-	// 1. Get all JS source files. They're potential controllers
-	// 		Currently are included only the ones with autofix!
-	// 2. Extract module name from XMLView
-	// 3. Find corresponding JS controller for each XMLView
-	// 4. Check the if controller has certain methods. Consider also inheritance chain
-	getClassNode(
+	methodExistsInController(
 		controllerName: string,
-		tsProgram: Program,
-		checker: ts.TypeChecker
+		methodName: string,
+		tsProgram?: Program,
+		checker?: ts.TypeChecker
 	) {
+		if (tsProgram === undefined || checker === undefined) {
+			return false;
+		}
+
 		const findClassDeclaration = (node: ts.Node): ts.ClassLikeDeclaration | undefined => {
 			if (ts.isClassLike(node)) {
 				return node;
@@ -53,19 +53,17 @@ export default class EventHandlersFix extends XmlEnabledFix {
 		const sourceFile = tsProgram.getSourceFile(controllerName);
 
 		if (!sourceFile) {
-			return;
+			return false;
 		}
 
 		const classDeclaration = findClassDeclaration(sourceFile);
 		let classType;
 		if (classDeclaration) {
 			classType = checker.getTypeAtLocation(classDeclaration);
-			console.log(classType.getProperty("onPressFancyButton")); // Current class method
-			console.log(classType.getProperty("onBaseControllerHandler")); // Inherited method
-			console.log(classType.getProperty("byId")); // sap.ui.core.mvc.Controller methods
+			return !!classType.getProperty(methodName);
 		}
 
-		return classType;
+		return false;
 	}
 
 	visitAutofixXmlNode(
@@ -76,11 +74,21 @@ export default class EventHandlersFix extends XmlEnabledFix {
 		const program = sharedLanguageService?.getProgram();
 		const checker = program?.getTypeChecker();
 
-		const classNode = this.getClassNode("/ambiguousEventHandlers/Main.controller.js", program, checker);
+		// TODO:
+		// 1. Extract module name from XMLView
+		// 2. Find corresponding JS controller for each XMLView
 
-		this.startPos = toPosition(node.name.start);
-		this.endPos = toPosition(node.value.end) + 1; // TODO: +1 might be incorrect if no quotes are used
-		return true;
+		const methodName = node.value.value.split("(")[0]; // If method with args, take just the name
+		const isAvailableMethod = this.methodExistsInController(
+			"/ambiguousEventHandlers/Main.controller.js", methodName, program, checker);
+
+		if (isAvailableMethod) {
+			this.methodName = node.value.value;
+			this.startPos = toPosition(node.value.start);
+			this.endPos = toPosition(node.value.end);
+		}
+
+		return isAvailableMethod;
 	}
 
 	visitAutofixNode(_node: ts.Node, _position: number, _sourceFile: ts.SourceFile) {
@@ -99,7 +107,13 @@ export default class EventHandlersFix extends XmlEnabledFix {
 	}
 
 	generateChanges(): ChangeSet | ChangeSet[] | undefined {
-		console.log("zzzz");
-		// throw new Error("Method 'generateChanges' must be implemented in subclasses");
+		if (this.methodName) {
+			return {
+				action: ChangeAction.REPLACE,
+				start: this.startPos!,
+				end: this.endPos!,
+				value: `.${this.methodName}`,
+			};
+		}
 	}
 }
