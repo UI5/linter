69b3344137827b63b0a7296dcee0a0a314969bd0
d3xter666 <yavor.ivanov@sap.com>
2025-08-12 10:56:57 +0300

    feat: ObjectLiterals with nested events in Controllers

--- test/fixtures/autofix/ambiguousEventHandlers/Base.controller.js
+++ test/fixtures/autofix/ambiguousEventHandlers/Base.controller.js
@@ -1,8 +1,13 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"],
-	function (Controller) {
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
 	"use strict";
 
 	return Controller.extend("ui5.walkthrough.controller.Base", {
+		baseEventHandlers: {
+			oneLevelDeeper: {
+				onPressButton: this.onBaseControllerHandler,
+			},
+		},
+
 		onBaseControllerHandler: function () {
 			// Handler logic
 		}
