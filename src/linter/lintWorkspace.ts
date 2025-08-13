1db8a34428c960c53858fdae719099960b02310e
d3xter666 <yavor.ivanov@sap.com>
2025-08-13 09:57:53 +0300

    fix: Forgotten sharedLanguageService.release()

--- src/linter/lintWorkspace.ts
+++ src/linter/lintWorkspace.ts
@@ -33,6 +33,7 @@ export default async function lintWorkspace(
 		workspace, filePathsWorkspace, options, config, patternsMatch, libraryDependencies, sharedLanguageService
 	);
 	if (!options.fix) {
+		sharedLanguageService.release();
 		return lintContext.generateLintResults();
 	}
 
