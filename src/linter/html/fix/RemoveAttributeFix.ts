d821368030f9071b9610ff8ea5a3cb99bd692710
Max Reichmann <max.reichmann@sap.com>
2025-08-13 10:58:38 +0200

    test: Enhance tests for unquoted attributes

--- src/linter/html/fix/RemoveAttributeFix.ts
+++ src/linter/html/fix/RemoveAttributeFix.ts
@@ -1,4 +1,4 @@
-import {Attribute, PositionDetail, Tag as SaxTag} from "sax-wasm";
+import {Attribute, AttributeType, PositionDetail, Tag as SaxTag} from "sax-wasm";
 import {ChangeAction, ChangeSet} from "../../../autofix/autofix.js";
 import {HtmlFix} from "./HtmlFix.js";
 import {ToPositionCallback} from "../../ui5Types/fix/XmlEnabledFix.js";
@@ -19,27 +19,91 @@ export default class RemoveAttributeFix extends HtmlFix {
 		this.endPositionDetail = endPos;
 	}
 
-	// TODO: Handle unquoted values correctly
+	// TODO: Remove
+	// calculateRemovalPositions(tag: SaxTag, attr: Attribute): {startPos: PositionDetail; endPos: PositionDetail} {
+	// 	// This will prepare the start and end pos of the removal.
+	// 	// If there is a previous attribute, we need to remove
+	// 	// the current attribute and all whitespace until the previous attribute.
+	// 	// This is needed to ensure there are no empty lines or whitespaces left after removal.
+	// 	const attrIndex = tag.attributes.indexOf(attr);
+	// 	const previousAttr = attrIndex > 0 ? tag.attributes[attrIndex - 1] : undefined;
+	// 	const startPos = previousAttr ?
+	// 			{
+	// 				line: previousAttr.value.end.line,
+	// 				character: previousAttr.value.end.character + 1,
+	// 			} :
+	// 			{
+	// 				line: attr.name.start.line,
+	// 				character: attr.name.start.character,
+	// 			};
+	// 	const endPos = {
+	// 		line: attr.value.end.line,
+	// 		character: attr.value.end.character + 1,
+	// 	};
+	// 	return {startPos, endPos};
+	// }
+
 	calculateRemovalPositions(tag: SaxTag, attr: Attribute): {startPos: PositionDetail; endPos: PositionDetail} {
+		let startPos, endPos: PositionDetail | undefined;
 		// This will prepare the start and end pos of the removal.
 		// If there is a previous attribute, we need to remove
 		// the current attribute and all whitespace until the previous attribute.
 		// This is needed to ensure there are no empty lines or whitespaces left after removal.
+
 		const attrIndex = tag.attributes.indexOf(attr);
 		const previousAttr = attrIndex > 0 ? tag.attributes[attrIndex - 1] : undefined;
-		const startPos = previousAttr ?
-				{
+
+		if (!previousAttr) {
+			// TODO: refactor (= attribute is first in tag)
+			startPos = {
+				line: attr.name.start.line,
+				character: attr.name.start.character,
+			};
+			endPos = {
+				line: attr.value.end.line,
+				character: attr.value.end.character + (attr.type === AttributeType.NoQuotes ? 0 : 1),
+			};
+		} else {
+			// TODO: add handling of JSX (try to handle, if not possible: catch & ignore)
+			// TODO: add handling of single-character attributes w/o values (eg: ,)
+			if (previousAttr.type === AttributeType.NoQuotes) {
+				startPos = {
 					line: previousAttr.value.end.line,
-					character: previousAttr.value.end.character + 1, // TODO: +1 depending on value being quoted or not
-				} :
-				{
-					line: attr.name.start.line,
-					character: attr.name.start.character,
+					character: previousAttr.value.end.character,
 				};
-		const endPos = {
-			line: attr.value.end.line,
-			character: attr.value.end.character + 1, // TODO: +1 depending on value being quoted or not
-		};
+			} else if (previousAttr.type === AttributeType.DoubleQuoted ||
+				previousAttr.type === AttributeType.SingleQuoted) {
+				startPos = {
+					line: previousAttr.value.end.line,
+					character: previousAttr.value.end.character + 1,
+				};
+			} else if (previousAttr.type === AttributeType.NoValue) {
+				startPos = {
+					line: previousAttr.name.end.line,
+					character: previousAttr.name.end.character,
+				};
+			}
+
+			if (attr.type === AttributeType.NoQuotes) {
+				endPos = {
+					line: attr.value.end.line,
+					character: attr.value.end.character,
+				};
+			} else if (attr.type === AttributeType.DoubleQuoted || attr.type === AttributeType.SingleQuoted) {
+				endPos = {
+					line: attr.value.end.line,
+					character: attr.value.end.character + 1,
+				};
+			} else if (attr.type === AttributeType.NoValue) {
+				endPos = {
+					line: attr.name.end.line,
+					character: attr.name.end.character,
+				};
+			}
+		}
+		if (!startPos || !endPos) {
+			throw new Error("Could not calculate start and end position for attribute removal");
+		}
 		return {startPos, endPos};
 	}
 
