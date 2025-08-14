6e0b086072a139c14bcf91c4810e9014fe20f38e
Max Reichmann <max.reichmann@sap.com>
2025-08-14 10:20:20 +0200

    test: Enhance test for RemoveAttributeFix

--- test/lib/linter/html/fix/RemoveAttributeFix.ts
+++ test/lib/linter/html/fix/RemoveAttributeFix.ts
@@ -1,3 +1,7 @@
+/* This test file is designed to test generic use cases of RemoveAttributeFix.
+These include not only UI5 use cases. We want to test the functionality of
+removing any kind of attribute inside any kind of HTML construct. */
+
 import anyTest, {TestFn} from "ava";
 import sinonGlobal from "sinon";
 import esmock from "esmock";
@@ -152,7 +156,210 @@ test("Remove Second Attribute from HTML tag", async (t) => {
 	t.is(Array.from(result.values())[0], expectedOutput, "Autofix output should match expected output");
 });
 
-// TODO: Test
-// - Remove multiple attributes
-// - Remove attributes with different amounts of whitespace
-// - Remove special attributes (e.g. no quotes, no value, single characters)
+test("Remove Multiple Attributes from HTML tag", async (t) => {
+	const input = `<!Doctype HTML>
+<html>
+<head>
+	<script remove="me"
+		keep="me"
+		remove="meToo">
+	</script>
+</head>
+<body>
+</body>
+</html>`;
+
+	const expectedOutput = `<!Doctype HTML>
+<html>
+<head>
+	<script
+		keep="me">
+	</script>
+</head>
+<body>
+</body>
+</html>`;
+
+	// ----- Parse and select attribute -----
+	const extractedTags = await _extractTagsFromString(input);
+	const scriptTag = extractedTags.scriptTags[0];
+	const attrToRemove1 = scriptTag.attributes[0];
+	const attrToRemove2 = scriptTag.attributes[2];
+
+	// ----- Create fix -----
+	const fix1 = new RemoveAttributeFix(scriptTag, attrToRemove1);
+	const fix2 = new RemoveAttributeFix(scriptTag, attrToRemove2);
+
+	// ----- Run Autofix -----
+	const result = await _runAutofix([fix1, fix2], input, t.context);
+
+	// ----- Compare Autofix output with expected output -----
+	t.truthy(result);
+	t.is(Array.from(result.values())[0], expectedOutput, "Autofix output should match expected output");
+});
+
+test("Remove Attributes with special syntax from HTML tag", async (t) => {
+	// This tests the handling of special syntax:
+	// no-quotes = no quotes around the value
+	// no-value = no value after the attribute name
+	// x = single character as attribute name
+	// , = single character as attribute name with no value
+
+	const input = `<!Doctype HTML>
+<html>
+<head>
+	<script no-quotes=abc
+		keep="me"
+		no-value
+		2keep="me"
+		x="remove"
+		3keep="me"
+		,>
+	</script>
+</head>
+<body>
+</body>
+</html>`;
+
+	const expectedOutput = `<!Doctype HTML>
+<html>
+<head>
+	<script
+		keep="me"
+		2keep="me"
+		3keep="me">
+	</script>
+</head>
+<body>
+</body>
+</html>`;
+
+	// ----- Parse and select attribute -----
+	const extractedTags = await _extractTagsFromString(input);
+	const scriptTag = extractedTags.scriptTags[0];
+	const attrToRemove1 = scriptTag.attributes[0];
+	const attrToRemove2 = scriptTag.attributes[2];
+	const attrToRemove3 = scriptTag.attributes[4];
+	const attrToRemove4 = scriptTag.attributes[6];
+
+	// ----- Create fix -----
+	const fix1 = new RemoveAttributeFix(scriptTag, attrToRemove1);
+	const fix2 = new RemoveAttributeFix(scriptTag, attrToRemove2);
+	const fix3 = new RemoveAttributeFix(scriptTag, attrToRemove3);
+	const fix4 = new RemoveAttributeFix(scriptTag, attrToRemove4);
+
+	// ----- Run Autofix -----
+	const result = await _runAutofix([fix1, fix2, fix3, fix4], input, t.context);
+
+	// ----- Compare Autofix output with expected output -----
+	t.truthy(result);
+	t.is(Array.from(result.values())[0], expectedOutput, "Autofix output should match expected output");
+});
+
+test("Remove Attributes with surrounding whitespace from HTML tag", async (t) => {
+	const input = `<!Doctype HTML>
+<html>
+<head>
+	<script   remove="me"
+		keep="me"
+		    remove="meToo">
+	</script>
+</head>
+<body>
+</body>
+</html>`;
+
+	const expectedOutput = `<!Doctype HTML>
+<html>
+<head>
+	<script
+		keep="me">
+	</script>
+</head>
+<body>
+</body>
+</html>`;
+
+	// ----- Parse and select attribute -----
+	const extractedTags = await _extractTagsFromString(input);
+	const scriptTag = extractedTags.scriptTags[0];
+	const attrToRemove1 = scriptTag.attributes[0];
+	const attrToRemove2 = scriptTag.attributes[2];
+
+	// ----- Create fix -----
+	const fix1 = new RemoveAttributeFix(scriptTag, attrToRemove1);
+	const fix2 = new RemoveAttributeFix(scriptTag, attrToRemove2);
+
+	// ----- Run Autofix -----
+	const result = await _runAutofix([fix1, fix2], input, t.context);
+
+	// ----- Compare Autofix output with expected output -----
+	t.truthy(result);
+	t.is(Array.from(result.values())[0], expectedOutput, "Autofix output should match expected output");
+});
+
+test("Remove Attributes with special whitespace from HTML tag", async (t) => {
+	// This tests the handling of whitespace between attribute names and values:
+	// x & y & z are single character attributes
+	const input = `<!Doctype HTML>
+<html>
+<head>
+	<script remove ="me"
+		keep="me"
+		remove= "meToo"
+		2keep="me"
+		remove = "meToo2"
+		3keep="me"
+		x ="remove"
+		4keep="me"
+		y= "remove"
+		5keep="me"
+		y = "remove"
+		6keep="me">
+	</script>
+</head>
+<body>
+</body>
+</html>`;
+
+	const expectedOutput = `<!Doctype HTML>
+<html>
+<head>
+	<script
+		keep="me"
+		2keep="me"
+		3keep="me"
+		4keep="me"
+		5keep="me"
+		6keep="me">
+	</script>
+</head>
+<body>
+</body>
+</html>`;
+
+	// ----- Parse and select attribute -----
+	const extractedTags = await _extractTagsFromString(input);
+	const scriptTag = extractedTags.scriptTags[0];
+	const attrToRemove1 = scriptTag.attributes[0];
+	const attrToRemove2 = scriptTag.attributes[2];
+	const attrToRemove3 = scriptTag.attributes[4];
+	const attrToRemove4 = scriptTag.attributes[6];
+	const attrToRemove5 = scriptTag.attributes[8];
+	const attrToRemove6 = scriptTag.attributes[10];
+
+	// ----- Create fix -----
+	const fix1 = new RemoveAttributeFix(scriptTag, attrToRemove1);
+	const fix2 = new RemoveAttributeFix(scriptTag, attrToRemove2);
+	const fix3 = new RemoveAttributeFix(scriptTag, attrToRemove3);
+	const fix4 = new RemoveAttributeFix(scriptTag, attrToRemove4);
+	const fix5 = new RemoveAttributeFix(scriptTag, attrToRemove5);
+	const fix6 = new RemoveAttributeFix(scriptTag, attrToRemove6);
+
+	// ----- Run Autofix -----
+	const result = await _runAutofix([fix1, fix2, fix3, fix4, fix5, fix6], input, t.context);
+
+	// ----- Compare Autofix output with expected output -----
+	t.truthy(result);
+	t.is(Array.from(result.values())[0], expectedOutput, "Autofix output should match expected output");
+});
