f07985a1cd215149d3c110d511a8a7bfcf8c4c66
Max Reichmann <max.reichmann@sap.com>
2025-08-16 11:05:33 +0200

    refactor: Improve RemoveAttributeFix

--- src/linter/html/fix/RemoveAttributeFix.ts
+++ src/linter/html/fix/RemoveAttributeFix.ts
@@ -19,91 +19,89 @@ export default class RemoveAttributeFix extends HtmlFix {
 		this.endPositionDetail = endPos;
 	}
 
-	// TODO: Remove
-	// calculateRemovalPositions(tag: SaxTag, attr: Attribute): {startPos: PositionDetail; endPos: PositionDetail} {
-	// 	// This will prepare the start and end pos of the removal.
-	// 	// If there is a previous attribute, we need to remove
-	// 	// the current attribute and all whitespace until the previous attribute.
-	// 	// This is needed to ensure there are no empty lines or whitespaces left after removal.
-	// 	const attrIndex = tag.attributes.indexOf(attr);
-	// 	const previousAttr = attrIndex > 0 ? tag.attributes[attrIndex - 1] : undefined;
-	// 	const startPos = previousAttr ?
-	// 			{
-	// 				line: previousAttr.value.end.line,
-	// 				character: previousAttr.value.end.character + 1,
-	// 			} :
-	// 			{
-	// 				line: attr.name.start.line,
-	// 				character: attr.name.start.character,
-	// 			};
-	// 	const endPos = {
-	// 		line: attr.value.end.line,
-	// 		character: attr.value.end.character + 1,
-	// 	};
-	// 	return {startPos, endPos};
-	// }
-
+	// This will prepare the start and end pos of the removal.
+	// This is needed to ensure there are no empty lines or whitespaces left after removal.
 	calculateRemovalPositions(tag: SaxTag, attr: Attribute): {startPos: PositionDetail; endPos: PositionDetail} {
 		let startPos, endPos: PositionDetail | undefined;
-		// This will prepare the start and end pos of the removal.
-		// If there is a previous attribute, we need to remove
-		// the current attribute and all whitespace until the previous attribute.
-		// This is needed to ensure there are no empty lines or whitespaces left after removal.
 
 		const attrIndex = tag.attributes.indexOf(attr);
 		const previousAttr = attrIndex > 0 ? tag.attributes[attrIndex - 1] : undefined;
 
 		if (!previousAttr) {
-			// TODO: add different handlings for all attribute types
-
+			// If there is no previous attribute (attribute is first), we remove the current attribute
+			// and all whitespace until the end of the tag name.
 			startPos = {
 				line: tag.openStart.line,
 				character: tag.openStart.character + tag.name.length + 1,
 			};
-			endPos = {
-				line: attr.value.end.line,
-				character: attr.value.end.character + (attr.type === AttributeType.NoQuotes ? 0 : 1),
-			};
 		} else {
-			// TODO: add handling of JSX (try to handle, if not possible: catch & ignore)
-			// TODO: add handling of single-character attributes w/o values (eg: ,)
-			if (previousAttr.type === AttributeType.NoQuotes) {
-				startPos = {
-					line: previousAttr.value.end.line,
-					character: previousAttr.value.end.character,
-				};
-			} else if (previousAttr.type === AttributeType.DoubleQuoted ||
-				previousAttr.type === AttributeType.SingleQuoted) {
-				startPos = {
-					line: previousAttr.value.end.line,
-					character: previousAttr.value.end.character + 1,
-				};
-			} else if (previousAttr.type === AttributeType.NoValue) {
-				startPos = {
-					line: previousAttr.name.end.line,
-					character: previousAttr.name.end.character,
-				};
+			// If there is a previous attribute, we remove the current attribute
+			// and all whitespace until the previous attribute.
+			switch (previousAttr.type) {
+				case AttributeType.NoQuotes:
+					startPos = {
+						line: previousAttr.value.end.line,
+						character: previousAttr.value.end.character,
+					};
+					break;
+				case AttributeType.SingleQuoted:
+				case AttributeType.DoubleQuoted:
+					startPos = {
+						line: previousAttr.value.end.line,
+						character: previousAttr.value.end.character + 1,
+					};
+					break;
+				case AttributeType.NoValue:
+					startPos = {
+						line: previousAttr.name.end.line,
+						character: previousAttr.name.end.character,
+					};
+					break;
 			}
-
-			if (attr.type === AttributeType.NoQuotes) {
+		}
+		switch (attr.type) {
+			case AttributeType.NoQuotes:
 				endPos = {
 					line: attr.value.end.line,
 					character: attr.value.end.character,
 				};
-			} else if (attr.type === AttributeType.DoubleQuoted || attr.type === AttributeType.SingleQuoted) {
+				break;
+			case AttributeType.SingleQuoted:
+			case AttributeType.DoubleQuoted:
 				endPos = {
 					line: attr.value.end.line,
 					character: attr.value.end.character + 1,
 				};
-			} else if (attr.type === AttributeType.NoValue) {
+				break;
+			case AttributeType.NoValue:
 				endPos = {
 					line: attr.name.end.line,
 					character: attr.name.end.character,
 				};
-			}
+				break;
 		}
-		if (!startPos || !endPos) {
-			throw new Error("Could not calculate start and end position for attribute removal");
+
+		// These two if statements are a workaround for edge cases
+		// since sax-wasm parses wrong positions for NoValue single-character attributes.
+		// TODO: Remove once it's fixed.
+		if (previousAttr && previousAttr.type === AttributeType.NoValue && previousAttr.name.value.length === 1) {
+			startPos = {
+				line: previousAttr.name.start.line,
+				character: previousAttr.name.start.character + 1,
+			};
+		}
+		if (attr.type === AttributeType.NoValue && attr.name.value.length === 1) {
+			endPos = {
+				line: attr.name.start.line,
+				character: attr.name.start.character + 1,
+			};
+		}
+
+		if (!startPos) {
+			throw new Error("Could not calculate start position for attribute removal");
+		}
+		if (!endPos) {
+			throw new Error("Could not calculate end position for attribute removal");
 		}
 		return {startPos, endPos};
 	}
